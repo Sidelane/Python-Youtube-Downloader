468b45cf187efa49063f13f4f958f6ea72b404f9
Sidelane <andreas.pelger@cit.fra-uas.de>
2020-11-29 03:26:25 +0100

    initial commit

--- helper/downloader.py
+++ helper/downloader.py
@@ -1,4 +1,5 @@
 import pytube
+import re
 from pytube.cli import on_progress
 from sys import exit
 from helper.converter import convert
@@ -28,11 +29,27 @@ def download_convert_video(link: str):
     except Exception as e:
         return e
 
+def download_playlist(links: list):
+    for link in links:
+        file = download_video(link)
+
+def download_convert_playlist(links: list):
+    for link in links:
+        file = download_convert_video(link)
+
 def download(link: str, playlist: bool, format: str):
     if playlist == False and format == "mp3":
         return download_convert_video(link)
     elif playlist == False and format == "mp4":
         return download_video(link)
+    elif playlist == True and format == "mp4":
+        pl = pytube.Playlist(link)
+        pl._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+        return download_playlist(pl.video_urls)
+    elif playlist == True and format == "mp3":
+        pl = pytube.Playlist(link)
+        pl._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+        return download_convert_playlist(pl.video_urls)
     else:
         print("error with video parameters to download")
         exit()
